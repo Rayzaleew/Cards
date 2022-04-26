7b4f81f78791700f71225b3f1148b560558fe103
Rayzaleew <104368253+Rayzaleew@users.noreply.github.com>
2022-04-27 03:23:42 +0700

    #last working version

--- ConsoleApplication2/Game.h
+++ ConsoleApplication2/Game.h
@@ -17,3 +17,25 @@ private:
 	vector<cards> hand;
 
 };
+
+
+
+class Game {
+private:
+	vector <cards> deck;
+	vector <string> names = { "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
+	vector <string> suits = { "Пики", "Крести", "Черви", "Буби" };
+	vector <cards> table;
+	string trump;
+	void building();
+	void shuffle();
+	void draft(vector <cards>& hand);
+	void trump_pick();
+	int deque(vector <cards>& hand);
+	void move(vector <cards>& hand, string name);
+	bool counter_move(vector <cards>& hand, string name);
+	bool toss(vector <cards>& hand);
+public:
+	void start(Player& first_player, Player& second_player);
+};
+
